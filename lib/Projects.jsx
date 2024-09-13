270cf1af3fd877db6c35e2b945a61fa4d9adaa87
puri <purip586@gmail.com>
2024-09-13 19:14:56 +0900

    cc

--- lib/Projects.jsx
+++ lib/Projects.jsx
@@ -1,44 +1,55 @@
 const projects = [
-    {
-      href: "https://xurealty-japan.com",
-      imageSrc: "/crossyou.jpg",
-      alt: "Cross You Real Estate Web App",
-      title: "A Real Estate Web App",
-      description: "Web app with property listing and management features.",
-      tags: ["NEXT", "Tailwind", "MongoDB", "AWS"],
-    },
-    {
-      href: "https://purip586.github.io/cross-u",
-      imageSrc: "/cross_you_landing.jpg",
-      alt: "Cross You Real Estate Web App",
-      title: "A Service Landing Page",
-      description: "An old version of landing page designed for Cross You Co. Ltd.",
-      tags: ["SCSS", "Javascript"],
-    },
-    {
-      href: "/",
-      imageSrc: "/innovait.jpg",
-      alt: "Innova IT Website",
-      title: "A Website for Innova IT",
-      description: "An Interactive website for an IT company featuring their business and services.",
-      tags: ["NEXT", "Tailwind", "Vercel"],
-    },
-    {
-      href: "https://apple-one-eosin.vercel.app/",
-      imageSrc: "/apple_website.png",
-      alt: "Apple Website",
-      title: "A cloned website of Apple",
-      description: "A cloned website for Apple company featuring their newly launched product iPhone 15 Pro.",
-      tags: ["React", "GSAP", "Tailwind", "Three.js"],
-    },
-    {
-      href: "/",
-      imageSrc: "/careplus_healthcare.png",
-      alt: "Healthcare Management App",
-      title: "A Healthcare Management App",
-      description: "A healthcare management app called Careplus",
-      tags: ["Next", "Appwrite", "Tailwind", "shadcn"],
-    },
-  ];
+  {
+    href: "https://xurealty-japan.com",
+    imageSrc: "/crossyou.jpg",
+    alt: "Cross You Real Estate Web App",
+    title: "A Real Estate Web App",
+    description: "Web app with property listing and management features.",
+    tags: ["NEXT", "Tailwind", "MongoDB", "AWS"],
+  },
+  {
+    href: "https://purip586.github.io/cross-u",
+    imageSrc: "/cross_you_landing.jpg",
+    alt: "Cross You Real Estate Web App",
+    title: "A Service Landing Page",
+    description:
+      "An old version of landing page designed for Cross You Co. Ltd.",
+    tags: ["SCSS", "Javascript"],
+  },
+  {
+    href: "/",
+    imageSrc: "/innovait.jpg",
+    alt: "Innova IT Website",
+    title: "A Website for Innova IT",
+    description:
+      "An Interactive website for an IT company featuring their business and services.",
+    tags: ["NEXT", "Tailwind", "Vercel"],
+  },
+  {
+    href: "https://apple-one-eosin.vercel.app/",
+    imageSrc: "/apple_website.png",
+    alt: "Apple Website",
+    title: "A cloned website of Apple",
+    description:
+      "A cloned website for Apple company featuring their newly launched product iPhone 15 Pro.",
+    tags: ["React", "GSAP", "Tailwind", "Three.js"],
+  },
+  {
+    href: "/",
+    imageSrc: "/careplus_healthcare.png",
+    alt: "Healthcare Management App",
+    title: "A Healthcare Management App",
+    description: "A healthcare management app called Careplus",
+    tags: ["Next", "Appwrite", "Tailwind", "shadcn"],
+  },
+  {
+    href: "/",
+    imageSrc: "/devoverflow",
+    alt: "A social app for developers",
+    title: "A social app for developers",
+    description: "A socail media app community for develpers to interact",
+    tags: ["Next", "Typescript", "Tailwind", "shadcn"],
+  },
+];
 
-  export default projects;
+export default projects;
